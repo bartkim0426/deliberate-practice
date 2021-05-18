85dfda3f5455ac26962c99ed9402720790a9651a
seulchankim <bartkim0426@gmail.com>
2021-05-19 02:07:03 +0900

    2021

--- exercises4programmers/ch01/python/tests.py
+++ exercises4programmers/ch01/python/tests.py
@@ -47,3 +47,82 @@ class CalculatorTestCase(TestCase):
         calls = [call('Tip: $1.69'), call('Total: $12.94')]
         mock_print.assert_has_calls(calls)
         self.assertEqual(result, (1.69, 12.94))
+
+    @patch('tip_calculator.input', create=True)
+    def test_enter_input(self, mock_input):
+        mock_input.side_effect = ['11.25', '15']
+        calculator = tip_calculator.TipCalculator()
+
+        # enter input from prompt
+        calculator._enter_input()
+
+        self.assertEqual(calculator.bill, '11.25')
+        self.assertEqual(calculator.tip_rate, '15')
+
+    @patch('tip_calculator.input', create=True)
+    def test_enter_input_with_invalid(self, mock_input):
+        mock_input.side_effect = ['invalid', '11.25', '15']
+        calculator = tip_calculator.TipCalculator()
+
+        # enter input from prompt
+        calculator._enter_input()
+
+        self.assertEqual(calculator.bill, '11.25')
+        self.assertEqual(calculator.tip_rate, '15')
+
+    def test_is_float(self):
+        calculator = tip_calculator.TipCalculator()
+
+        self.assertFalse(calculator._is_float('not_float'))
+        self.assertTrue(calculator._is_float('1.11'))
+
+    def test_is_validate_for_valid_value(self):
+        calculator = tip_calculator.TipCalculator()
+
+        calculator.bill = '1.1'
+        calculator.tip_rate = '15'
+
+        calculator._validate()
+
+    def test_is_validate_for_invalid_value(self):
+        calculator = tip_calculator.TipCalculator()
+
+        calculator.bill = 'not_valid'
+        calculator.tip_rate = '15'
+
+        with self.assertRaises(ValueError):
+            calculator._validate()
+
+    def test_calculate_tip(self):
+        calculator = tip_calculator.TipCalculator()
+        calculator.bill = '11.25'
+        calculator.tip_rate = '15'
+
+        calculator._calculate_tip()
+
+        self.assertEqual(calculator.tip, 1.69)
+        self.assertEqual(calculator.total, 12.94)
+
+    @patch('builtins.print')
+    def test_print_output(self, mock_print):
+        calculator = tip_calculator.TipCalculator()
+        calculator.bill = '11.25'
+        calculator.tip_rate = '15'
+
+        calculator._calculate_tip()
+        calculator._print_output()
+
+        calls = [call('Tip: $1.69'), call('Total: $12.94')]
+        mock_print.assert_has_calls(calls)
+
+    @patch('builtins.print')
+    @patch('tip_calculator.input')
+    def test_proceed(self, mock_input, mock_print):
+        '''test proceed whole input -> output process'''
+        mock_input.side_effect = ['11.25', '15']
+        calculator = tip_calculator.TipCalculator()
+        # proceed input to output
+        calculator.proceed()
+
+        calls = [call('Tip: $1.69'), call('Total: $12.94')]
+        mock_print.assert_has_calls(calls)
--- exercises4programmers/ch01/python/tip_calculator.py
+++ exercises4programmers/ch01/python/tip_calculator.py
@@ -6,10 +6,10 @@ tip calculator
     price: int
 - [o] output: rounding in cents
 
-+ [ ] 가격과 팁 비율 값으로 숫자만 받게
-+ [ ] 잘못 입력된 값에 대해 메세지 출력하고 프로그램을 종료하는 대신 허용되는 값이 입력될 때까지 계속 입력요청
-+ [ ] 음수 값이 입력되지 않도록
-+ [ ] 연산 부분을 함수로 처리하도록 분할
++ [o] 가격과 팁 비율 값으로 숫자만 받게
++ [o] 잘못 입력된 값에 대해 메세지 출력하고 프로그램을 종료하는 대신 허용되는 값이 입력될 때까지 계속 입력요청
++ [o] 음수 값이 입력되지 않도록
++ [o] 연산 부분을 함수로 처리하도록 분할
 + [ ] GUI 버전으로 개선. 입력 값이 바뀌면 결과값이 자동으로 반영
 + [ ] 팁 비율을 퍼센트 단위로 입력받는 대신 슬라이더를 사용하여 조절할 수 있도록 수정. 슬라이더의 범위는 5~20%
 
@@ -36,6 +36,7 @@ TipCalculator
     Display "Total: $" + total
 End
 '''
+from dataclasses import dataclass
 
 
 def calculate_tip() -> tuple:
@@ -57,3 +58,95 @@ def calculate_tip() -> tuple:
     print("Total: $" + str(total))
     
     return tip, total
+
+
+@dataclass
+class TipDataClass:
+    bill: float
+    tip_rate: int
+    tip: float
+    total: float
+
+    def __init__(self, bill, tip_rate):
+        self.bill = bill
+        self.tip_rate = tip_rate
+        self._calculate_tip(bill, tip_rate)
+
+    def _calculate_tip(self, bill: float, tip_rate: int):
+        '''set tip and total after calculate with bill and tip_rate'''
+        self.tip = round(bill * (tip_rate / 100), ndigits=2)
+        self.total = self.bill + self.tip
+
+
+class TipCalculator:
+    '''
+    Tip calculator from bill and tip_rate
+
+    arguments:
+        bill: float
+        tip_rate: int
+
+    interfaces:
+        _validate
+        calculate_tip
+        enter_input
+        print_output
+    '''
+    def __init__(self):
+        pass
+        # self._enter_input()
+
+    def _enter_input(self):
+        '''
+        enter input for bill and tip_rate from prompt
+
+        Continuously receive input if the input value is invalid
+        '''
+        self.bill: str = input('What is the bill amount? ')
+        while not self._is_float(self.bill):
+            print('Wrong bill. Bill should be number or float only.')
+            self.bill: str = input('What is the bill amount? ')
+
+        self.tip_rate: str = input('What is the tip rate? ')
+        while not self.tip_rate.isdigit():
+            print('Wrong tip_rate. Tip_rate should be number')
+            self.tip_rate: str = input('What is the tip rate? ')
+
+    def _print_output(self):
+        '''print output of tip and total bill'''
+        print(f'Tip: ${self.tip}')
+        print(f'Total: ${self.total}')
+
+    def _is_float(self, x: str) -> bool:
+        '''check if input x is float or not'''
+        try:
+            float(x)
+        except ValueError:
+            return False
+        return True
+
+    def _validate(self):
+        try:
+            assert self._is_float(self.bill)
+            assert self.tip_rate.isdigit()
+        except AssertionError as e:
+            raise ValueError(f'Invalid Value: {e}')
+
+    def _calculate_tip(self):
+        bill = float(self.bill)
+        tip_rate = int(self.tip_rate)
+        tip_data = TipDataClass(bill=bill, tip_rate=tip_rate)
+
+        self.tip = tip_data.tip
+        self.total = tip_data.total
+
+    def proceed(self):
+        '''proceed calculator by getting input from prompt and output to prompt'''
+        self._enter_input()
+        self._validate()
+        self._calculate_tip()
+        self._print_output()
+
+
+if __name__ == '__main__':
+    TipCalculator().proceed()
